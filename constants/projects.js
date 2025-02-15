f3b0bc51e387a5b48f8ba0cedd131a08327179a8
Awais12xd <lucifer.awaisxx213@example.com>
2025-02-15 12:04:45 +0500

    first commit

---
+++ constants/projects.js
@@ -0,0 +1,67 @@
+import project1 from "@/public/images/projects/project1.PNG";
+import project2 from "@/public/images/projects/project2.PNG";
+import project3 from "@/public/images/projects/project3.PNG";
+import project4 from "@/public/images/projects/project4.PNG";
+import css from "styled-jsx/css";
+
+export const featuredProjects = [
+  {
+    name: "Queens-Pint",
+    image: project1,
+    detail: "Queens-Pint is a UK-based fast food restaurant offering delicious meals with quick service, designed to enhance the user experience through a seamless and engaging web interface.",
+    tech: ["React.js", "JavaScript", "Tailwind CSS"],
+    webUrl: "https://queens-pint.netlify.app/",
+    githubUrl: "",
+  },
+  {
+    name: "Ezitech-Institute",
+    image: project2,
+    detail: "Ezitech-Institute provides remote internships to students, and I designed its responsive and user-friendly frontend using modern web technologies.",
+    tech: ["React.js", "JavaScript", "Tailwind CSS"],
+    webUrl: "https://ezitech-institute.netlify.app/",
+    githubUrl: "",
+  },
+  {
+    name: "RAW-etc",
+    image: project3,
+    detail: "RAW-etc is an e-commerce platform that sells stylish hats, with a smooth shopping experience through an optimized and visually appealing interface.",
+    tech: ["React.js", "JavaScript", "Tailwind CSS"],
+    webUrl: "https://hatstore.netlify.app/shop",
+    githubUrl: "",
+  },
+  {
+    name: "Feelo",
+    image: project4,
+    detail: "Feelo is a platform offering food delivery and hostel room bookings, with a clean and intuitive frontend design for effortless user interaction.",
+    tech: ["React.js", "JavaScript", "Tailwind CSS"],
+    webUrl: "https://feelo.netlify.app/",
+    githubUrl: "",
+  },
+];
+
+
+export const projects = [
+  {
+    name: "Currency Converter",
+    detail: "A web application that allows users to convert between various currencies in real-time using live exchange rates, ensuring accurate and quick conversions.",
+    tech: ["React.js", "JavaScript", "Tailwind CSS"],
+    webUrl: "https://currency-convertxx.netlify.app/",
+    githubUrl: ""
+  },
+  {
+    name: "Password Generator",
+    detail: "An intuitive tool that generates strong, random passwords based on user-selected criteria such as length, symbols, numbers, and letters.",
+    tech: ["React.js", "JavaScript", "Tailwind CSS"],
+    webUrl: "https://generate-react.netlify.app/",
+    githubUrl: ""
+  },
+  {
+    name: "Todo List App",
+    detail: "A simple and effective task management app that helps users organize their tasks, set priorities, and track their progress effortlessly.",
+    tech: ["React.js", "JavaScript", "Tailwind CSS"],
+    webUrl: "https://react-todo-123-app.netlify.app/",
+    githubUrl: ""
+  }
+ 
+];
+
